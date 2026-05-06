5234eb472001a44852535635ff52c6be18498820
sumitverma00000000654165-tech <sumitverma00000000654165@gmail.com>
2026-05-07 00:49:36 +0530

    55

---
+++ script.js
@@ -0,0 +1,20 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBeZl52HEWGz-vE72QZ5taznvpYTfq3QUk",
+    authDomain: "login-and-reg-5728d.firebaseapp.com",
+    databaseURL: "https://login-and-reg-5728d-default-rtdb.firebaseio.com",
+    projectId: "login-and-reg-5728d",
+    storageBucket: "login-and-reg-5728d.firebasestorage.app",
+    messagingSenderId: "435846403197",
+    appId: "1:435846403197:web:d6a05a55e40839d15dcd14"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
